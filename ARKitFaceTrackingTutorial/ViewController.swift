248156b69a1290cb44581a63bfeba80327e353c2
Evgeniy Antonov <antonoff.evgeniy@gmail.com>
2019-04-23 09:21:14 +0300

    Initial commit

---
+++ ARKitFaceTrackingTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARKitFaceTrackingTutorial
+//
+//  Created by Evgeniy Antonov on 4/23/19.
+//  Copyright © 2019 Evgeniy Antonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
